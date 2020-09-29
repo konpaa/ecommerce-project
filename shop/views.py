b81839185b86b8c85e67b5d691fa7c3c19b143dc
Pavel <pashakonoplyanikov@gmail.com>
2020-09-29 14:53:18 +0300

    catrgory in dropdown

--- shop/views.py
+++ shop/views.py
@@ -5,15 +5,15 @@ from .models import Category, Product
 # Create your views here.
 
 
-def home(request, category_slug=None):
+def home(request, category_slug=None): 
 	category_page = None
 	products = None  
-	if category_slug != None:
+	if category_slug != None: 
 		category_page = get_object_or_404(Category, slug = category_slug)
-		products = Product.object.filter(category = category_page, available = True)
+		products = Product.objects.filter(category = category_page, available = True)
 	else:
 		products = Product.objects.all().filter(available = True)
-	return render(request, 'home.html',{'category':category_page,'products' : products})
+	return render(request, 'home.html',{'category' : category_page,'products' : products})
 
-def product(request):
-	return render(request, 'product.html')
+def product(request):    
+	return render(request, 'product.html') 
