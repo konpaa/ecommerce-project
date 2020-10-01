f453e041827d423935765f5a62e36af4bfe24518
Pavel <pashakonoplyanikov@gmail.com>
2020-10-01 15:55:00 +0300

    cart

--- shop/models.py
+++ shop/models.py
@@ -15,10 +15,12 @@ class Category(models.Model):
 		verbose_name_plural = 'categories'
 
 	def get_url(self):
+
 		return reverse('products_by_category', args=[self.slug])
 			
 
 	def __str__(self):
+		
 		return self.name
 
 
@@ -38,6 +40,12 @@ class Product(models.Model):
 		ordering = ('name',)
 		verbose_name = 'product'
 		verbose_name_plural = 'products'
+
+
+	def get_url(self):
+
+		return reverse('product_detail', args=[self.category.slug,self.slug])
+
 
 	def __str__(self):
 		return self.name
